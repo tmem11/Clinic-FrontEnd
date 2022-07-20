589205593aff948dea7903270e867538d1df32cf
tmem11 <tmem.nsasra@gmail.com>
2022-07-21 00:51:45 +0300

    adding map and locating kesefa on map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { MapContainer, TileLayer,Marker,Popup } from 'react-leaflet'
+import L, { icon } from 'leaflet'
+const clinicMarker = new L.Icon({
+  iconUrl: require('./imgs/hospital-building.png'),
+  iconSize:[35,45],
+})
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <MapContainer center={[31.242986, 35.084657]} zoom={15} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[31.25051027149744, 35.08253561201448]} icon={clinicMarker} >
+    <Popup>
+      clalit
+    </Popup>
+  </Marker>
+</MapContainer>
+   
   );
 }
 
